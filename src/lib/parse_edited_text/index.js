1990b51a0fc33fa906cce0fb22268930ef7c42f6
pietrop <pietro.passarelli@gmail.com>
2017-11-21 17:38:21 +0800

    update last paragraph missing

--- src/lib/parse_edited_text/index.js
+++ src/lib/parse_edited_text/index.js
@@ -1,17 +1,23 @@
 'use strict';
 
+ var words = /\w/;
+
 function parse(text){
 	var results = [];
 	var sequence = {};
 	//split on single lin ebreak 
-	var lines = text.split("\n");
+	// using trim to remove trailing spaces/empty lines at the end. 
+	var lines = text.trim().split("\n");
+
 	// iterate over lines. 
 	for(var i=0; i< lines.length ; i++){
 		var line = lines[i];
 		//if it's not an empty line 
 		if(line !==""){
+
 			// `~` used to identify lines with speakers 
 			if(line.includes("~")){
+
 				// skip if first line to add to results the sequence. 
 				if(i!== 0){
 					// saves new sequence, when identified next speaker
@@ -22,22 +28,28 @@ function parse(text){
 				sequence.speaker = line.replace(/~/g, "");
 				//initialises text for sequence
 				sequence.text = "";
-			}else{
+			}else if(words.test(line)){
 				//adds text, to cover edge case where multiple lines of text in between speakers
 				sequence.text += line;
-				// console.log(line);
+
+				//identify the last element.
+				//because we add the sequence when identify a new speaker, we risk missing out the last "paragraph" if we don't add this last part to include it to the results
+				if(i == (lines.length -1)){
+					// console.log(sequence)
+					// add last element to sequence 
+					results.push(sequence);
+				}	
 			}
 		}
 	}
 
 
-
+	//adding an `seq` attribute to number the new lines. 
 	results.forEach(function(s, index){
 		s.seq = index; 
 	});
-
 	return results;
-};
+};	
 
 
 //Making a list of peakers without timecodes, and text, but with sequence number
