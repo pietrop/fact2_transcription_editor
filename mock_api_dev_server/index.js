0315f2efd7401c5b6127b6e62c0939520f03f493
pietrop <pietro.passarelli@gmail.com>
2018-03-07 20:00:08 +0000

    added summort for m3u8
    if media type is m3u8 in transcription json then it will change the video media type inthe the view video element to support playback works in safari not in chrome for now

--- mock_api_dev_server/index.js
+++ mock_api_dev_server/index.js
@@ -40,7 +40,7 @@ app.put('/api/proofreader.php', function(req, res) {
 
 //load sample data
 //var sampleJsonTranscription = require('./sample3.json');
-var sampleJsonTranscription = require("./BAH-2018-02-05.json");
+var sampleJsonTranscription = require("./m3u8_sample.json");
 
 //enable cross origin request  - https://enable-cors.org/server_expressjs.html
 // app.use(function(req, res, next) {
